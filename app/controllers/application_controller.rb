437efae58b45377eee9c42731be73d0957f79245
Adrian082818 <mohabiradrian@gmail.com>
2020-02-20 20:13:57 -0500

    added helper methods to applications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,40 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "stupid"
   end
 
   get "/" do
     erb :welcome
   end
+
+  helpers do 
+     
+    def logged_in?
+      !!current_user
+    end 
+
+    def login(username, password)
+      user = User.find_by(:username => username)
+
+      if user && user.authenticate(password)
+        session[:username] = user.username
+        session[:user_id] = user.id
+      else 
+        redirect '/login'
+      end 
+    end 
+
+    def current_user
+      @current_user ||= User.find_by(:username => session[:username]) if session[:username]
+    end
+
+    def logout 
+      session.clear
+    end 
+
+    end 
+  end 
 
 end
